742899bd3655bc1b94e82fc9c60cbc6c10bbd4e1
Dalnec <dalnec14@gmail.com>
2023-03-03 10:56:10 -0500

    one bk

--- main.py
+++ main.py
@@ -5,6 +5,27 @@ from send_drive import download_file, list_items, searchFile, uploadFile
 from tools import printProgressBar
 
 
+def do_one_backup(name):
+    f = open("logs.txt", "a")
+    databases = get_databases()
+    l = len(databases)
+    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    index = 0
+    for database in databases:
+        index+=1
+        db, owner = database
+        if db == name:
+            file_name, gz_file = create_file(db, owner)
+            time.sleep(1)
+            os.remove(file_name)
+            id_drive_db = uploadFile(gz_file, gz_file, "application/gzip")
+            f.write(f"{index}: {db} \t=> {id_drive_db}\n")
+            os.remove(gz_file)
+            time.sleep(1)
+        printProgressBar(index, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    f.close()
+
 def do_backups():
     f = open("logs.txt", "a")
     databases = get_databases()
@@ -28,9 +49,10 @@ def do_backups():
 def main():
     print("\nOPTIONS:")
     print("1.- Show DataBases")
-    print("2.- Generate Backups")    
-    print("3.- List Backups drive")    
-    print("4.- Download Backup drive")    
+    print("2.- Generate One Backup")    
+    print("3.- Generate Backups")    
+    print("4.- List Backups drive")    
+    print("5.- Download Backup drive")    
     print("0.- Exit")    
     option = int(input("Enter a number:"))
 
@@ -38,12 +60,16 @@ def main():
         showDBs()
         main()
     elif option == 2:
-        do_backups()
+        name = input("Enter name:")
+        do_one_backup(name)
         main()
     elif option == 3:
-        list_items()
+        do_backups()
         main()
     elif option == 4:
+        list_items()
+        main()
+    elif option == 5:
         name = input("Enter name:")
         file_id, file_name = searchFile(100, name, '', 'id')
         download_file(file_id, file_name)
