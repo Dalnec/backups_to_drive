96a8e96ff051eb1f252ba26465164e5047ff106b
Dalnec <dalnec1405@gmail.com>
2024-04-27 21:04:31 -0400

    updating reqs

--- send_drive.py
+++ send_drive.py
@@ -7,24 +7,39 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
+from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.appdata'] #.metadata.readonly']
 
 def getCredentials():
     creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    # if os.path.exists('token.pickle'):
+    #     with open('token.pickle', 'rb') as token:
+    #         creds = pickle.load(token)
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'credenciales/credentials_faqture.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     with open('token.pickle', 'wb') as token:
+    #         pickle.dump(creds, token)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credenciales/credentials_faqture.json', SCOPES)
+                'credenciales/credentials_faqture.json', SCOPES
+            )
             creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
     service = build('drive', 'v3', credentials=creds, cache_discovery=False, static_discovery=False)
     return service
